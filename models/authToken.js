c8a7446f4c9b451fad80db0409461d706fdd3d9b
Brian Hoff <blhoff52002@gmail.com>
2019-09-26 10:46:28 -0500

    fixing

--- models/authToken.js
+++ models/authToken.js
@@ -1,35 +1,38 @@
-// module.exports = (sequelize, DataTypes) => {
+module.exports = (sequelize, DataTypes) => {
+  const AuthToken = sequelize.define(
+    "AuthToken",
+    {
+      token: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+    },
+    {}
+  );
+  AuthToken.associate = function({ User }) {
+    AuthToken.belongsTo(User);
+  };
 
-//     const AuthToken = sequelize.define('AuthToken', {
-//       token: {
-//         type: DataTypes.STRING,
-//         allowNull: false,
-//       }
-//     }, {});
-//     AuthToken.associate = function({ User }) {
-//       AuthToken.belongsTo(User);
-//     };
+  // generates a random 15 character token and
+  // associates it with a user
+  AuthToken.generate = async function(UserId) {
+    if (!UserId) {
+      throw new Error("AuthToken requires a user ID");
+    }
 
-//     // generates a random 15 character token and
-//     // associates it with a user
-//     AuthToken.generate = async function(UserId) {
-//       if (!UserId) {
-//         throw new Error('AuthToken requires a user ID')
-//       }
+    let token = "";
 
-//       let token = '';
+    const possibleCharacters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "abcdefghijklmnopqrstuvwxyz0123456789";
 
-//       const possibleCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' +
-//         'abcdefghijklmnopqrstuvwxyz0123456789';
+    for (var i = 0; i < 15; i++) {
+      token += possibleCharacters.charAt(
+        Math.floor(Math.random() * possibleCharacters.length)
+      );
+    }
 
-//       for (var i = 0; i < 15; i++) {
-//         token += possibleCharacters.charAt(
-//           Math.floor(Math.random() * possibleCharacters.length)
-//         );
-//       }
+    return AuthToken.create({ token, UserId });
+  };
 
-//       return AuthToken.create({ token, UserId })
-//     }
-
-//     return AuthToken;
-// };
+  return AuthToken;
+};
