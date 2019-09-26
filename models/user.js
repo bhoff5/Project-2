b08e214a67db7fb0ba147667171abbe11ecd41d8
Brian Hoff <blhoff52002@gmail.com>
2019-09-26 10:37:40 -0500

    fixing

--- models/user.js
+++ models/user.js
@@ -1,73 +1,72 @@
-var bcrypt = require("bcrypt");
+// var bcrypt = require("bcrypt");
 
-module.exports = function (sequelize, DataTypes) {
-    var User = sequelize.define("User", {
-        userName: {
-            type: DataTypes.STRING,
-            unique: true,
-            allowNull: false
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        admin: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false
-        },
-        createdAt: {
-            type: DataTypes.DATE,
-            allowNull: false,
-            defaultValue: sequelize.fn("NOW")
-        },
-        updatedAt: {
-            type: DataTypes.DATE,
-            allowNull: false,
-            defaultValue: sequelize.fn("NOW")
-        }
-    });
+// module.exports = function (sequelize, DataTypes) {
+//     var User = sequelize.define("User", {
+//         userName: {
+//             type: DataTypes.STRING,
+//             unique: true,
+//             allowNull: false
+//         },
+//         password: {
+//             type: DataTypes.STRING,
+//             allowNull: false
+//         },
+//         admin: {
+//             type: DataTypes.BOOLEAN,
+//             allowNull: false
+//         },
+//         createdAt: {
+//             type: DataTypes.DATE,
+//             allowNull: false,
+//             defaultValue: sequelize.fn("NOW")
+//         },
+//         updatedAt: {
+//             type: DataTypes.DATE,
+//             allowNull: false,
+//             defaultValue: sequelize.fn("NOW")
+//         }
+//     });
 
-    User.associate = function(models) {
-        User.hasMany(models.AuthToken);
-    };
+//     User.associate = function(models) {
+//         User.hasMany(models.AuthToken);
+//     };
 
-    User.authenticate = async function(userName, password) {
+//     User.authenticate = async function(userName, password) {
 
-        const user = await User.findOne({ where: { userName } });
-    
-        if (bcrypt.compareSync(password, user.password)) {
-            return user.authorize();
-        }
-    
-        throw new Error('invalid password');
-      }
-    
-      User.prototype.authorize = async function () {
-        const { AuthToken } = sequelize.models;
-        const user = this
-    
-        // create a new auth token associated to 'this' user
-        // by calling the AuthToken class method we created earlier
-        // and passing it the user id
-        const authToken = await AuthToken.generate(this.id);
-    
-        // addAuthToken is a generated method provided by
-        // sequelize which is made for any 'hasMany' relationships
-        await user.addAuthToken(authToken);
-    
-        return { 
-            userId: user.id,
-            userName: user.userName,
-            token: authToken.token
-        }
-      };
-    
-    
-      User.prototype.logout = async function (token) {
-    
-        // destroy the auth token record that matches the passed token
-        sequelize.models.AuthToken.destroy({ where: { token } });
-      };
-    
-    return User;
-}
+//         const user = await User.findOne({ where: { userName } });
+
+//         if (bcrypt.compareSync(password, user.password)) {
+//             return user.authorize();
+//         }
+
+//         throw new Error('invalid password');
+//       }
+
+//       User.prototype.authorize = async function () {
+//         const { AuthToken } = sequelize.models;
+//         const user = this
+
+//         // create a new auth token associated to 'this' user
+//         // by calling the AuthToken class method we created earlier
+//         // and passing it the user id
+//         const authToken = await AuthToken.generate(this.id);
+
+//         // addAuthToken is a generated method provided by
+//         // sequelize which is made for any 'hasMany' relationships
+//         await user.addAuthToken(authToken);
+
+//         return {
+//             userId: user.id,
+//             userName: user.userName,
+//             token: authToken.token
+//         }
+//       };
+
+//       User.prototype.logout = async function (token) {
+
+//         // destroy the auth token record that matches the passed token
+//         sequelize.models.AuthToken.destroy({ where: { token } });
+//       };
+
+//     return User;
+// }
